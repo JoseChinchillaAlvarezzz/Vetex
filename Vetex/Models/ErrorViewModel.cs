9ff645ea2374bf4b38e47c4399c36041649f0806
JoseChinchillaAlvarezzz <jose.chinchilla@catolica.edu.sv>
2025-09-12 10:27:14 -0600

    Agregar archivos de proyecto.

---
+++ Vetex/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Vetex.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
