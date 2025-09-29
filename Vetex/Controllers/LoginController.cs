0072668ac2becd480b74c9e2d0eb13b35268fa2c
CarcasXD <carlossalesianoprro@gmail.com>
2025-09-29 10:25:18 -0600

    Creacion de controlador del modulo Login y diseño principales a usar del controlador

---
+++ Vetex/Controllers/LoginController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vetex.Controllers
+{
+    public class LoginController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult menuInicial() 
+        {
+            return View();
+        }
+    }
+}
