90c52593d68a53d086ced92a17de5c93925e10b0
aecheverria29 <tuemail@example.com>
2025-09-30 13:58:13 -0600

    Creacion de controlador del modulo de registro y diseno de pantallas a usar en el controlador

---
+++ Vetex/Controllers/RegistroController.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Win32;
+using Vetex.Models;
+
+namespace Vetex.Controllers
+{
+    public class RegistroController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult List()
+        {
+            return View();
+        }
+
+        public IActionResult History()
+        {
+            return View();
+        }
+
+        public IActionResult Details()
+        {
+            return View();
+        }
+
+        public IActionResult Create()
+        {
+            return View();
+        }
+    }
+}
