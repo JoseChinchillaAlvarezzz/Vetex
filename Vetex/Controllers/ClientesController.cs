1dbfc006096906e0ac759e717be3ee8b9f59cddc
Gabriel Moreno <Gabrielmorenoxv@gmail.com>
2025-09-29 22:07:35 -0600

    Creacion del modulo clientes y diseño de vistas que se usaran en el controlador

---
+++ Vetex/Controllers/ClientesController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vetex.Controllers
+{
+    public class ClientesController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+
+        }
+        public IActionResult Registro_Dueno()
+        {
+            return View();
+        }
+        public IActionResult Editar_Datos()
+        {
+            return View();
+        }
+    }
+}
