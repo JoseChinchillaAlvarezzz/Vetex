5e4c5d57d549acd6569a02c008c266e945e2aadd
nogus <nogus19lopez@gmail.com>
2025-09-29 22:49:00 -0600

    Creacion de controlador del modulo de citas y diseño de pantallas a usar en el controlador

---
+++ Vetex/Controllers/CitasController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace Vetex.Controllers
+{
+    public class CitasController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Crear_cita() { 
+        return View();
+        }
+    }
+}
