5f01b6ef6b8485fac2e8de77039667b8e1b24921
aecheverria29 <tuemail@example.com>
2025-11-04 10:45:08 -0600

    Implementacion completa de vista Create y controlador Registro

---
+++ Vetex/Models/RegistroCreateDTO.cs
@@ -0,0 +1,27 @@
+﻿namespace Vetex.Models
+{
+    public class RegistroMedDTO
+    {
+        public int MedicamentoId { get; set; }
+        public int Cantidad { get; set; }
+        public int DosisHoras { get; set; }
+    }
+
+    public class RegistroCreateDTO
+    {
+        // Relación principal
+        public int MascotaId { get; set; }
+
+        // Ficha clínica (según tu entidad)
+        public string? Resena { get; set; }
+        public string? Motivo { get; set; }
+        public decimal Temperatura { get; set; }
+        public int PulsoId { get; set; }
+        public int RespiracionId { get; set; }
+        public int DeshidratacionId { get; set; }
+        public string? Diagnostico { get; set; }
+
+        // Prescripciones
+        public List<RegistroMedDTO> Medicamentos { get; set; } = new();
+    }
+}
